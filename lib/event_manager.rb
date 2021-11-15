c8ae2087d438e700a170d745870729c3e9ce1a4d
dawidl022 <domdaw7@gmail.com>
2021-11-15 18:53:32 +0000

    Iteration 1: CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,23 @@
+require 'csv'
+
+DATA_FILE_NAME = 'event_attendees.csv'
+
 puts "Event Manager Initialised!\n\n"
+
+if !File.exist?(DATA_FILE_NAME)
+  exit
+end
 
 puts 'Names of attendees:'
 
-if File.exist?('event_attendees.csv')
-  lines = File.readlines('event_attendees.csv')
-  lines[1..].each do |line|
-    puts line.split(",")[2]
-  end
+contents = CSV.open(
+  DATA_FILE_NAME,
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
