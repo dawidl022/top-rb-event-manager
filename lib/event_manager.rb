e60be6ffd24c07833abbb79cbafc677be0a3960b
dawidl022 <domdaw7@gmail.com>
2021-11-15 20:19:10 +0000

    Iteration 4: Personal letters using templating

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,10 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 DATA_FILE_NAME = 'event_attendees.csv'
+TEMPLATE_FILE_NAME = 'form_letter.erb'
+OUTPUT_DIR = 'output'
 
 def clean_zipcode(zipcode)
   zipcode.to_s[0, 5].rjust(5, "0")
@@ -16,20 +19,27 @@ def legislators_by_zipcode(zipcode)
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    ).officials.map(&:name).join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting ' \
     'www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-puts "Event Manager Initialised!\n\n"
+def save_thank_you_letter(id, form_letter)
+  file_name = "output/thanks_#{id}.html"
+
+  File.open(file_name, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts "Event Manager Initialised!"
 
 if !File.exist?(DATA_FILE_NAME)
   exit
 end
 
-puts 'Names of attendees:'
 
 contents = CSV.open(
   DATA_FILE_NAME,
@@ -37,11 +47,19 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read(TEMPLATE_FILE_NAME)
+erb_template = ERB.new(template_letter)
+
+Dir.mkdir(OUTPUT_DIR) unless Dir.exist?(OUTPUT_DIR)
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
